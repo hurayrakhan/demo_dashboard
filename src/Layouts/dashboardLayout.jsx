e39c4d46186449c4ac30bd387862568d25c91c3d
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-07-29 23:57:28 +0600

    added dashboard layout

---
+++ src/Layouts/dashboardLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const dashboardLayout = () => {
+    return (
+        <div>
+            hello dashboard
+            
+        </div>
+    );
+};
+
+export default dashboardLayout;
